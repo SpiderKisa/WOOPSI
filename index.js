9494ea4c16dc74287c968e149c65477baf17bc29
Alexandra Skopina <skopina.aa@outlook.com>
2022-08-23 15:21:15 +1000

    using mongo sanitize

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const ExpressError = require('./utilities/ExpressError');
 
@@ -33,6 +34,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'assets')));
+app.use(mongoSanitize());
 
 const sessionOptions = {
     secret: 'secretSecretSecret',
@@ -52,9 +54,6 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-// app.use('/jquery', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
-
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
